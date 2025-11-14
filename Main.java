3949acc797593eb33e925099baa462c20abbeb1c
Mikel <mperezramo@educacion.navarra.es>
2025-11-14 17:39:34 +0100

    Cambio de saludo en mejoras

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Hola soy Mikel desde la versión principal!");
     }
 }
